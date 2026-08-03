6c8f127c7c91372137f3b35b3fa8a15108212b80
Yann Ramin <github@theatr.us>
2026-08-03 12:22:28 -0700

    Update seiza-mac cask to 0.6.2 (#7)

--- Casks/seiza-mac.rb
+++ Casks/seiza-mac.rb
@@ -1,6 +1,6 @@
 cask "seiza-mac" do
-  version "0.6.1"
-  sha256 "05484d2053e97175b1355702e58ff9fadb07d44df9780b96cac2a61f0c854df1"
+  version "0.6.2"
+  sha256 "b51fdf2a59d9b7113624c5e96718c7e136f210e066a9f23952713827103b9fc0"
 
   url "https://github.com/theatrus/seiza-mac/releases/download/v#{version}/Seiza-#{version}-universal.dmg"
   name "Seiza for Mac"
@@ -20,8 +20,10 @@ cask "seiza-mac" do
   zap trash: [
     "~/Library/Application Scripts/fyi.seiza.mac",
     "~/Library/Application Scripts/fyi.seiza.mac.quicklook",
+    "~/Library/Application Scripts/fyi.seiza.mac.thumbnail",
     "~/Library/Containers/fyi.seiza.mac",
     "~/Library/Containers/fyi.seiza.mac.quicklook",
+    "~/Library/Containers/fyi.seiza.mac.thumbnail",
     "~/Library/Saved Application State/fyi.seiza.mac.savedState",
   ]
 end
